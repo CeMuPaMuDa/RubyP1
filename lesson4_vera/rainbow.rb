12389c09ca09731d24640e6e88b49a0908a3ea1a
CeMuPaMuDa <bepyc9i@gmail.com>
2021-06-11 00:23:38 +0300

    add reynbow hash

---
+++ lesson4_vera/rainbow.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+colors = {
+  red: 'красный',
+  orange: 'оранжевый',
+  yellow: 'желтый',
+  green: 'зеленый',
+  blue: 'голубой',
+  indigo: 'синий',
+  violet: 'фиолетовый'
+}
+
+puts "Ключи: #{colors.keys.join(', ')}"
+puts "Значения: #{colors.values.join(', ')}"
