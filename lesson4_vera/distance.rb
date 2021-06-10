0f83c6689f5f1158b2be7f6bf916c8466b8bdff9
CeMuPaMuDa <bepyc9i@gmail.com>
2021-06-11 02:34:59 +0300

    rubocop -A

--- lesson4_vera/distance.rb
+++ lesson4_vera/distance.rb
@@ -2,6 +2,6 @@
 
 a = { x: 3, y: 7 }; b = { x: -1, y: 5 }
 
-result = Math.sqrt(((b[:x] - a[:x]) ** 2) + ((b[:y] - a[:y]) ** 2))
+result = Math.sqrt(((b[:x] - a[:x])**2) + ((b[:y] - a[:y])**2))
 
 puts "Paccтояние между точками: #{result.round(2)}"
