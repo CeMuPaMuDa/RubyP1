715907e3423ac2cec8ac1f802f82396173e33740
CeMuPaMuDa <bepyc9i@gmail.com>
2021-06-17 23:01:43 +0300

    task 1 complate

---
+++ lesson6_vera/lib/day_of_week.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+MONDAY = 'Понедельник'
+TUESDAY = 'Вторник'
+WEDNESDAY = 'Среда'
+THURSDAY = 'Четверг'
+FRIDAY = 'Пятница'
+SATURDAY = 'Суббота'
+SUNDAY = 'Воскресенье'
---
+++ lesson6_vera/week.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require_relative 'lib/day_of_week'
+
+WEEK = [MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY].freeze
+
+puts WEEK
