01549c292970d35e42f6c5af892ac645adcc51db
CeMuPaMuDa <bepyc9i@gmail.com>
2021-06-18 15:48:37 +0300

    task 3 complate

--- lesson6_vera/hello.rb
+++ lesson6_vera/hello.rb
@@ -1,7 +1,22 @@
 # frozen_string_literal: true
 
 class Hello
-  def say_hello; end
+  def initialize(hour)
+    @hour = hour
+  end
+
+  def say_hello
+    if @hour >= 6 && @hour < 12
+      'Доброе утро!'
+    elsif @hour >= 12 && @hour < 18
+      'Добрый день!'
+    elsif @hour >= 18 && @hour <= 23
+      'Добрый вечер!'
+    else
+      'Доброй ночи!'
+    end
+  end
 end
 
-puts Time.now
+what_time = Hello.new(Time.new.hour)
+puts what_time.say_hello
