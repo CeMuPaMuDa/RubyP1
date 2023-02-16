39a7579c4ed95ef66ffdb4ea0b943d3636bc24ee
CeMuPaMuDa <bepyc9i@gmail.com>
2023-02-16 13:40:18 +0200

    updated task 1, class Ticket moved to lib folder

--- lesson2/1_ticket.rb
+++ lesson2/1_ticket.rb
@@ -1,26 +1,5 @@
 # frozen_string_literal: true
-
-class Ticket
-  def date
-    '27.10.2021'
-  end
-
-  def price
-    80
-  end
-
-  def row
-    4
-  end
-
-  def seat
-    12
-  end
-
-  def event
-    'Трудно быть рубистом'
-  end
-end
+require_relative '../lib/ticket'
 
 ticket = Ticket.new
 
---
+++ lib/ticket.rb
@@ -0,0 +1,21 @@
+class Ticket
+  def date
+    '27.10.2021'
+  end
+
+  def price
+    80
+  end
+
+  def row
+    4
+  end
+
+  def seat
+    12
+  end
+
+  def event
+    'Трудно быть рубистом'
+  end
+end
